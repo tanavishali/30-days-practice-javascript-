4659a9bceefc0d0b061eaea1102a85d78a55eb42
tanavishali <tanawishalrai5271@gmail.com>
2024-07-27 12:00:40 +0500

    day one

---
+++ day-one/script.js
@@ -0,0 +1,74 @@
+"use strict";
+// Write a program that checks if a number is positive, negative, or zero.
+/*
+let num = Number(prompt("enter number"));
+console.log(num, typeof num);
+if (num == 0) {
+  console.log(`${num}: is not negative or positive ${num} is zero`);
+} else if (num > 0) {
+  console.log(`${num}: is positive`);
+} else if (num < 0) {
+  console.log(`${num}: is negative`);
+} else {
+  console.log("invalid input");
+}
+*/
+
+// Write a program that checks if a number is even or odd.
+/*
+let num = Number(prompt("enter number"));
+console.log(num, typeof num);
+if (num % 2 == 0) {
+  console.log(`${num}: is even`);
+} else {
+  console.log(`${num}: is odd`);
+}
+*/
+//3: Write a program that calculates the grade based on marks.
+/*
+let num = Number(prompt("enter your marks "));
+console.log(num, typeof num);
+if (num >= 90 && num < 100) {
+  console.log(`your marks ${num}: and  your grade is A+`);
+} else if (num >= 80 && num < 90) {
+  console.log(`your marks ${num}: and  your grade is A`);
+} else if (num >= 70 && num < 80) {
+  console.log(`your marks ${num}: and  your grade is B`);
+} else if (num >= 60 && num < 70) {
+  console.log(`your marks ${num}: and  your grade is C`);
+} else if (num >= 50 && num < 60) {
+  console.log(`your marks ${num}: and  your grade is D`);
+} else if (num < 50) {
+  console.log(`${num}: you fail try next time`);
+} else {
+  console.log("invalid input");
+}
+*/
+
+// 4: Write a program that checks if a year is a leap year.
+/*
+let num = Number(prompt("year "));
+console.log(num, typeof num);
+if ((num % 4 == 0 && num % 100 != 0) || num % 400 == 0) {
+  console.log("leap year");
+} else {
+  console.log("not leap year");
+}
+*/
+// 5:Write a program that checks if a character is a vowel or consonant.
+/*
+let char = prompt("enter character for check vowel or not")..toLowerCase();
+console.log(typeof char);
+if (
+  char === "a" ||
+  char === "e" ||
+  char === "i" ||
+  char === "o" ||
+  char === "u"
+) {
+  console.log(`${char} :vowel`);
+} else {
+  console.log(`${char}: consonant`);
+}
+*/
+// 6:Write a program that classifies a person's age group.
