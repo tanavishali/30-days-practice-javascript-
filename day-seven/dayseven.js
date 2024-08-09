c8eb24291bf6faa01daf72da0212027664dcbcf3
tanavishali <tanawishalrai5271@gmail.com>
2024-08-09 23:03:18 +0500

    day 7

---
+++ day-seven/dayseven.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+let numbers = [1, 2, 3, 4, 5, 6];
+let sum = 0;
+let sumofarray = [];
+for (let i = 0; i < numbers.length; i++) {
+  console.log(sumofarray.push(numbers[i]));
+  sum = sum + numbers[i];
+}
+console.log(sum);
+console.log(sumofarray);
+let showsum = (document.querySelector(".sum").textContent = sum);
+*/
